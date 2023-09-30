fa8f997b6b7ad76c094edb3271e09211f80463ef
MaxPiccinno <maxpiccinno18@gmail.com>
2023-09-30 15:39:01 +0200

    attachments section

--- src/app/pages/form-page/form-page.component.ts
+++ src/app/pages/form-page/form-page.component.ts
@@ -10,6 +10,7 @@ export class FormPageComponent implements OnInit {
 
   mainForm!: FormGroup;
   lessons!: FormArray;
+  attachments!: FormArray;
 
   ngOnInit(): void {
     this.initForm();
@@ -20,17 +21,29 @@ export class FormPageComponent implements OnInit {
     this.lessons = this.mainForm.get('lessons') as FormArray;
   }
 
+  addAttachmentControl() {
+    (this.mainForm.get('attachments') as FormArray).controls.push(this.initAttachmentControl());
+    this.attachments = this.mainForm.get('attachments') as FormArray;
+  }
+
   removeLessonSection(index: number) {
     (this.mainForm.get('lessons') as FormArray).removeAt(index);
     this.lessons = this.mainForm.get('lessons') as FormArray;
   }
 
+  removeAttachmentControl(index: number) {
+    (this.mainForm.get('attachments') as FormArray).removeAt(index);
+    this.attachments = this.mainForm.get('attachments') as FormArray;
+  }
+
   private initForm() {
     this.mainForm = new FormGroup({
       name: new FormControl<string>('', [Validators.required]),
-      lessons: new FormArray([this.initLessonForm()])
+      lessons: new FormArray([this.initLessonForm()]),
+      attachments: new FormArray([this.initAttachmentControl()])
     });
     this.lessons = this.mainForm.get('lessons') as FormArray;
+    this.attachments = this.mainForm.get('attachments') as FormArray;
   }
 
   private initLessonForm(): FormGroup {
@@ -38,6 +51,10 @@ export class FormPageComponent implements OnInit {
       name: new FormControl<string>('', [Validators.required]),
       file: new FormControl<File>(null, [Validators.required])
     })
+  }
+
+  private initAttachmentControl(): FormControl {
+    return new FormControl<File>(null);
   }
 
 }
